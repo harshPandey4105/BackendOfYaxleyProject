7f3c6b2d2b53edbac2588b4e51a0f28bcde4dd61
Harsh Padey <gurukuldeveloper16@gmail.com>
2024-02-23 01:56:22 -0800

    add some env setup setting

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 require("./db/config");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
